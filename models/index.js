8d09ae393f323edc3005ebea09c8138f2d85b4f1
BrianPizz <bpizzimentijr@outlook.com>
2023-10-17 12:52:34 -0700

    updated through table

--- models/index.js
+++ models/index.js
@@ -17,12 +17,18 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
+  through: {
+    model: ProductTag,
+    uniqe: false
+},
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
+  through: {
+    model: ProductTag,
+    uniqe: false
+},
 });
 
 module.exports = {
